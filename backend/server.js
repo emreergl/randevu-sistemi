5f458d951e3a7d704071e8d15fc1c9a0239ae2da
emreergl <emrergul30@gmail.com>
2026-09-10 10:51:48 +0300

    PostgreSQL'e geçiş tamamlandı: Neon veritabanı bağlantısı kuruldu

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const app = express();
 const  { startReminderJob } = require("./utils/reminderService");
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: true,
     credentials: true
 }));
 app.use(express.json());
